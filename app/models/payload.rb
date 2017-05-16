6fbad511e372fe834f07d9a32e7656812b7ff558
Bryan Bibat <bryan@aelogica.com>
2017-05-17 07:35:17 +0800

    Add more event info in index

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,2 +1,17 @@
 class Payload < ApplicationRecord
+  def timestamp
+    begin
+      Time.at(JSON.parse(body)["created"])
+    rescue JSON::ParserError
+      ""
+    end
+  end
+
+  def event_id
+    begin
+      JSON.parse(body)["id"]
+    rescue JSON::ParserError
+      ""
+    end
+  end
 end
